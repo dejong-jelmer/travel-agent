ddcf4c2a1c705147e5dd9885b139f633a2c3713a
dejong-jelmer <18470899+dejong-jelmer@users.noreply.github.com>
2025-03-28 08:41:32 +0000

    Apply automatic changes

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,6 +1,7 @@
 <?php
-if(! function_exists('randomPrice')) {
-    function randomPrice(int $min = 200, int $max = 5000): String
+
+if (! function_exists('randomPrice')) {
+    function randomPrice(int $min = 200, int $max = 5000): string
     {
         return (string) fake()->randomFloat(2, $min, $max);
     }
