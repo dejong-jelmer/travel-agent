9dab6eb2932a272e68444ef848a4c51927883f91
dejong-jelmer <18470899+dejong-jelmer@users.noreply.github.com>
2025-04-23 19:20:06 +0000

    Apply automatic changes

--- app/Providers/FakerPhoneServiceProvider.php
+++ app/Providers/FakerPhoneServiceProvider.php
@@ -13,17 +13,21 @@ class FakerPhoneServiceProvider extends ServiceProvider
         $this->app->singleton(Generator::class, function () {
             $faker = FakerFactory::create();
 
-            $faker->addProvider(new class($faker) extends \Faker\Provider\PhoneNumber {
-                private function randFristDigitBetween($min, $max, $count) {
-                    return rand($min, $max) . $this->numerify(str_repeat('#', $count - 1));
+            $faker->addProvider(new class($faker) extends \Faker\Provider\PhoneNumber
+            {
+                private function randFristDigitBetween($min, $max, $count)
+                {
+                    return rand($min, $max).$this->numerify(str_repeat('#', $count - 1));
                 }
 
-                public function validDutchMobileNumber() {
-                    return '+316' . $this->randFristDigitBetween(1, 5, 8);
+                public function validDutchMobileNumber()
+                {
+                    return '+316'.$this->randFristDigitBetween(1, 5, 8);
                 }
 
-                public function validBelgianMobileNumber() {
-                    return '+324' . $this->randFristDigitBetween(5, 9, 8);
+                public function validBelgianMobileNumber()
+                {
+                    return '+324'.$this->randFristDigitBetween(5, 9, 8);
                 }
             });
 
