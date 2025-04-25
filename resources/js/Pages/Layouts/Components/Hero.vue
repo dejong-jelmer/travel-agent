8cf3e7b7592abc1be013c82a16e71db2d054a110
jelmer de jong <j.de.jong@tutanota.com>
2025-04-25 11:37:33 +0200

    Move static images to public folder

--- resources/js/Pages/Layouts/Components/Hero.vue
+++ resources/js/Pages/Layouts/Components/Hero.vue
@@ -21,7 +21,7 @@ const { output: typedText } = useTypeWriter (subtitle);
 </script>
 
 <template>
-    <div class="h-screen bg-cover bg-top flex text-white px-6 bg-[url('/images/website/hero-image.jpg')]">
+    <div class="h-screen bg-cover bg-top flex text-white px-6 bg-[url('/images/hero-image.jpg')]">
     <div class="relative max-w-screen-desktop w-full mx-auto">
       <div class="absolute top-[35%] tablet:top-[45%] left-[10%] tablet:left-[20%]">
         <h1 class="text-4xl tablet:text-5xl font-semibold leading-10 tablet:leading-[72px]">
