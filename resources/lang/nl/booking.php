1c44f9c5c7890e1ff20b143e8ff009974c3e9fe4
Jelmer <j.de.jong@tutanota.com>
2025-11-20 12:02:13 +0100

    Fix typo

--- resources/lang/nl/booking.php
+++ resources/lang/nl/booking.php
@@ -2,6 +2,6 @@
 
 return [
     'created' => 'Je boeking is geslaagd! In je mail ontvang je een bevestiging en met meer details voor je aanstaande reis.',
-    'updated' => 'De aan passing op boeking :reference is geslaagd.',
+    'updated' => 'De aanpassing op boeking :reference is geslaagd.',
     'destroyed' => 'De boeking is verwijderd.',
 ];
